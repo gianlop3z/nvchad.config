0b842bd13abbfff2c3a50f8bab78abd1cccb065a
Gian Lopez <lopezarizagianlucas@gmail.com>
2023-07-08 16:04:25 -0500

    [mappings]: agregada la clonación de cursores con `vim-visual-multi`

--- init.lua
+++ init.lua
@@ -1,14 +1,19 @@
 -- snippets
 vim.g.lua_snippets_path = vim.fn.stdpath "config" .. "/lua/custom/snippets"
 
--- vim-move
-vim.g.move_key_modifier = "C"
-vim.g.move_key_modifier_visualmode = "C"
-
 -- pydocstring
 vim.g.pydocstring_doq_path = "/Library/Frameworks/Python.framework/Versions/3.11/bin/doq"
 vim.g.pydocstring_formatter = "google"
 
+-- vim-move
+vim.g.move_normal_option = 1
+
+-- vim-visual-multi
+vim.g.VM_maps = {
+  ["Add Cursor Up"] = "<S-Up>",
+  ["Add Cursor Down"] = "<S-Down>",
+}
+
 -- diagnostics
-vim.o.updatetime = 1000
+vim.o.updatetime = 2000
 vim.cmd [[autocmd! CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]]
--- mappings.lua
+++ mappings.lua
@@ -2,37 +2,21 @@ local M = {}
 
 M.disabled = {
   n = {
-    ["<C-s>"] = "", -- disable save file, it's <leader>s now
-    ["<C-j>"] = "", -- move line down override
-    ["<C-k>"] = "", -- move line up override
-    ["<C-h>"] = "", -- move line left override
-    ["<C-l>"] = "", -- move line right override
-    ["<C-n>"] = "", -- toogle nvimtree
-    ["<leader>n"] = "", -- toogle line number
+    ["<C-s>"] = "", -- "Save file" changed to <leader>s
+    ["<C-n>"] = "", -- "Toggle nvimtree" changed to <leader>e
+    ["<leader>n"] = "", -- "Toggle line numbers" changed to "Toggle nvimtree"
   },
 }
 
 M.general = {
   n = {
-    [";"] = { ":", "enter command mode", opts = { nowait = true } },
-    ["<leader>,"] = { "<C-w>l", "window right" },
-    ["<leader>s"] = { "<cmd>w<CR>", "save file" },
+    [";"] = { ":", "Enter command mode", opts = { nowait = true } },
+    ["<leader>,"] = { "<C-w>l", "Focus current right buffer" },
+    ["<leader>s"] = { "<cmd>w<CR>", "Save file" },
+    ["<leader>ro"] = { "<cmd>SearchReplaceSingleBufferOpen<CR>", "Search and replace in current buffer" },
+    ["<leader>oi"] = { "<cmd>TSOrganizeImports<CR>", "Organize imports in typescript files" },
     ["<leader>n"] = { "<cmd>NvimTreeToggle<CR>" },
     ["<leader>ax"] = { "<cmd>%bd | e# | NvimTreeOpen<CR>" },
-    ["<leader>ro"] = { "<cmd>SearchReplaceSingleBufferOpen<CR>", "search and replace in open buffer" },
-    ["<leader>oi"] = { "<cmd>TSOrganizeImports<CR>", "organize imports in `typescript` files" },
-    ["<leader>[t"] = {
-      function()
-        require("todo-comments").jump_next()
-      end,
-      "jump to next `todo-comments` comment",
-    },
-    ["<leader>]t"] = {
-      function()
-        require("todo-comments").jump_prev()
-      end,
-      "jump to previous `todo-comments` comment",
-    },
   },
 }
 
